8702c3425433d4252966bab1ed2a3513dc2bcc20
gline9 <gline9@comcast.net>
2019-10-06 21:49:58 -0500

    Adds final account unit tests

--- backend/finance-help/src/test/groovy/com/gline/finance/account/InMemoryAccountStoreSpec.groovy
+++ backend/finance-help/src/test/groovy/com/gline/finance/account/InMemoryAccountStoreSpec.groovy
@@ -25,4 +25,30 @@ class InMemoryAccountStoreSpec extends Specification
 
         1 == accountStore.getAccounts().size()
     }
+
+    void "should replace account state properly"()
+    {
+        given:
+
+        def accountStore = new InMemoryAccountStore()
+
+        def accountCreationMemento = Memento.emptyBean()
+        accountCreationMemento.setProperty("balance", 100d)
+        accountCreationMemento.setProperty("rate", 1)
+        accountCreationMemento.setProperty("compoundsPerYear", 1)
+        def accountCreation = AccountCreationRequest.fromMomento(accountCreationMemento)
+        accountStore.createAccount(accountCreation)
+
+
+        when:
+
+        def accounts = accountStore.getSerializableData()
+
+        accountStore.createAccount(accountCreation)
+        accountStore.setFromMementos(accounts*.serializeToMemento())
+
+        then:
+
+        1 == accountStore.getAccounts().size()
+    }
 }
